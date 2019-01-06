cdb9ca81ac18f8ac7e373ba190d3a8e6a065c6ef
permanar <richie.permana@gmail.com>
2019-01-07 01:11:35 +0800

    Adding routes for API

--- routes/api.php
+++ routes/api.php
@@ -11,8 +11,20 @@ use Illuminate\Http\Request;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::post('login', 'UserController@login');
+Route::post('register', 'UserController@register');
+
+Route::group(['middleware' => 'auth:api'], function () {
+    Route::resource('/mahasiswa', 'MahasiswaController');
+    Route::resource('/matakuliah', 'MahasiswaController');
+    Route::resource('/matakuliahdetails', 'CategoryController');
+
+    Route::get('/mahasiswa', 'MahasiswaController@tasks');
+    Route::get('/matakuliah', 'CategoryController@tasks');
 });
